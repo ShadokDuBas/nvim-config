7ce43820671cadccb2f9a4e6fa57333c72f8c711
Vincent Peth <vincent.peth@free.fr>
2024-11-20 13:55:49 +0100

    Updated the dashboard

--- lua/plug_list/dashboard.lua
+++ lua/plug_list/dashboard.lua
@@ -26,10 +26,24 @@ Non, vraiment, je suis à l’intérieur de l’ordinateur, sortez-moi de là…
 
 return {
     enabled = true,
-    preset = {header = art},
+    preset = {
+    header = art,
+    keys = {
+      { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+      { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+      { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+      { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+      { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+      { icon = " ", key = "G", desc = "Git", action = ":Git" },
+      { icon = " ", key = "t", desc = "Capture Task", action = "<leader>oc" },
+      { icon = " ", key = "a", desc = "Agenda", action = "<leader>oa" },
+      { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy",},-- enabled = package.loaded.lazy ~= nil },
+      { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+    }
+  },
     sections = {
       { section = "header",  },
-      { section = "keys", gap = 1, padding = 6, pane = 2 },
+      { section = "keys", gap = 1, padding = 2, pane = 2 },
       { section = "startup", pane = 2 },
     },
 }
--- lua/plug_list/pomodoro.lua
+++ lua/plug_list/pomodoro.lua
@@ -3,10 +3,6 @@ return {
   version = "*",  -- Recommended, use latest release instead of latest commit
   lazy = true,
   cmd = { "TimerStart", "TimerRepeat", "TimerSession" },
-  dependencies = {
-    -- Optional, but highly recommended if you want to use the "Default" timer
-    "rcarriga/nvim-notify",
-  },
   opts = {
   },
 }
