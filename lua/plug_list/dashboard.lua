1208379c86d7a4ffe6a38e1b61c832e0b1a10b57
Vincent Peth <vincent.peth@free.fr>
2024-11-24 16:45:00 +0100

    changed the dashboard shortcuts

--- lua/plug_list/dashboard.lua
+++ lua/plug_list/dashboard.lua
@@ -31,10 +31,10 @@ return {
     keys = {
       { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
       { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-      { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+      { icon = " ", key = "/", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
       { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
       { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-      { icon = " ", key = "G", desc = "Git", action = ":Git" },
+      { icon = " ", key = "g", desc = "Git", action = ":Git" },
       { icon = " ", key = "t", desc = "Capture Task", action = "<leader>oc" },
       { icon = " ", key = "a", desc = "Agenda", action = "<leader>oa" },
       { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy",},-- enabled = package.loaded.lazy ~= nil },
