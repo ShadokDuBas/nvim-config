453d265cce2070d7600c999137bfd8ce0c621662
Vincent Peth <vincent.peth@free.fr>
2024-12-18 15:05:43 +0100

    Split options and keymaps

--- lua/options.lua
+++ lua/options.lua
@@ -61,29 +61,6 @@ vim.opt.showmode = false
 -- Signcolumn, used to display signs(???)
 vim.opt.signcolumn = "yes"
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-
--- Window navigation
-vim.keymap.set("n", "<C-left>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-right>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-down>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-up>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-
--- Highlight when yanking text
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
 
 -- considérer que tex = latex
 vim.g.tex_flavor = "latex"
@@ -93,22 +70,5 @@ vim.g.tex_flavor = "latex"
 -- un fichier md.
 vim.opt.conceallevel = 0
 
--- fermer le buffer sans fermer la fenêtre
-vim.keymap.set("n", "<leader>q", ":bp<bar>sp<bar>bn<bar>bd<CR>", 
-  {desc = "[q]uit buffer, but keep window"})
 
--- Chercher-remplacer le mot sous le curseur
-vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>//gc<Left><Left><Left>]], 
-  {desc = "search and [R]eplace word under cursor"})
 
--- zen mode
-vim.keymap.set("n", "<leader>z", 
-  function() 
-    local ok, snack = pcall(require, "snacks") 
-    if not ok then
-      vim.notify("erreur dans le module zen :")
-      vim.notify(snack)
-    else snack.zen.zen()
-    end
-  end, 
-  {desc="activate zen mode"})
