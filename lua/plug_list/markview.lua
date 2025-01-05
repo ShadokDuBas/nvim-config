33cb89fb9c4a35b8daefa6a6a664bd528ca86cbc
Vincent Peth <vincent.peth@free.fr>
2025-01-05 16:34:55 +0100

    Setup hybrid mode for markview

--- lua/plug_list/markview.lua
+++ lua/plug_list/markview.lua
@@ -1,9 +1,14 @@
 return {
-    "OXY2DEV/markview.nvim",
-    lazy = false,      -- Recommended
-    -- ft = "markdown" -- If you decide to lazy-load anyway
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-        "nvim-tree/nvim-web-devicons"
+  "OXY2DEV/markview.nvim",
+  lazy = false,      -- Recommended
+  -- ft = "markdown" -- If you decide to lazy-load anyway
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons"
+  },
+  config = function()
+    require("markview").setup{
+      hybrid_modes = {"n"}
     }
+  end,
 }
