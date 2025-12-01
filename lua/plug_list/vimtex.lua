8a8cd70bdfef5ddb683f0eca74b1cb63c7677d99
Vincent <vincent.peth@free.fr>
2025-12-01 14:58:55 +0100

    Tried to make the document viewer better (did not work)

--- lua/plug_list/vimtex.lua
+++ lua/plug_list/vimtex.lua
@@ -2,8 +2,11 @@ return {
   "lervag/vimtex",
   lazy = false,     -- we don't want to lazy load VimTeX
   -- tag = "v2.15", -- uncomment to pin to a specific release
-  init = function()
+  config = function()
     -- VimTeX configuration goes here, e.g.
     -- vim.g.vimtex_view_method = "Okular" -- does not work ???
+    -- vim.g.vimtex_view_general_options = "--new-window @pdf";
+    vim.g.vimtex_view_general = "evince";
+    
   end
 }
